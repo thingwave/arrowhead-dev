0a83795f50dff35953317b5d5d6c0fd07101af7e
Jens Eliasson <jens.eliasson@gmail.com>
2022-08-05 20:01:45 +0200

    added HTTP support

--- Python/LocalCloudClient/lcclient.py
+++ Python/LocalCloudClient/lcclient.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
+import sys
+import argparse
+import requests
 
+def main(argv):
+    print('Eclipse Arrowhead Local cloud HTTP(S) client tool\nCopyright 2022 ThingWave AB')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cloud', type=str, required=False, help='Local clound certificate file')
+    parser.add_argument('--cert', type=str, required=False, help='System certificate file')
+    parser.add_argument('--key', type=str, required=False, help='System certificate key file')
+    parser.add_argument('--insecure',  action="store_true", required=False, help='To run in INSECURE mode')
+    parser.add_argument('--uri', type=str, required=True, help='URI to call')
+    args = parser.parse_args()
 
-def main():
-    print('Eclipse Arrowhead Local cloud HTTPS client tool\nCopyright 2022 ThingWave AB')
-
+    if args.insecure == True:
+        try:
+            r = requests.get(url=args.uri, params=None)
+            print(r)
+        except:
+            print("Could not connect to: '" + args.uri + "'")
+    
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
 
